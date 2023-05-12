f6494dd99f7c4424ccb9cf98bfce130779f0fb81
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-05-12 15:50:08 -0400

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,14 +1,8 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
-
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
-})
-
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
-
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -22,38 +16,35 @@ router.get('/:id', (req, res) => {
   }
 })
 
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-     res.render('error404')
-  }
-  else if (!places[id]) {
-     res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] })
-  }
-})
 
 router.delete('/:id',(req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
     res.render('error404')
-  }
-  else if (!places[id]) {
+  } else if (!places[id]) {
     res.render('error404')
-  }
-  else {
+  } else {
     places.splice(id, 1)
     res.redirect('/places')
   }
 })
-  
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  } else if (!places[id]) {
+    res.render('error404')
+  } else {
+    res.render ('places/edit', { place: places[id]})
+  }
+})
+
 router.post('/', (req, res) => {
   if (!req.body.pic) {
     // Default image if one is not provided
     req.body.pic = 'http://placekitten.com/400/400'
-  }
+  } 
   if (!req.body.city) {
     req.body.city = 'Anytown'
   }
@@ -63,6 +54,31 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    if (!req.body.pic){
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state ='USA'
+    }
+    places[id] =req.body
+    res.redirect(`/places/${id}`)
+  }
+})
+
+
 router.get('/', (req, res) => {
   res.render('places/index', { places })
 })
