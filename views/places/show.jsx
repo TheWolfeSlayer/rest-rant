3d51b1c92593a1cefcfa87b2529e7df2da4269c4
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-05-12 12:51:15 -0400

    delete and edit button made

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,26 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                Edit
+            </a>
+            <form method='POST' action={`places/${data.id}?_method=DELETE`}>
+                <button type='submit' className='btn btn-danger'>
+                    Delete
+                </button>
+            </form>
+            <h2> Rating</h2>
+            <p>Unrated</p>
+            <h2>Description</h2>
+            <p>{`Located in ${data.place.city}, ${data.place.state} and serves ${data.place.cuisines}`}</p>
+            <h2>Comments</h2>
+            <p>No comments found</p>
+            <img src={ data.place.pic } width={450}/>
           </main>
         </Def>
     )
